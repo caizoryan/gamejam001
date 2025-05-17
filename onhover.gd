0eebc4e54b875e4208f224142933d3b7d22268e6
Aaryan Pashine <pashineaaryan@gmail.com>
2025-05-17 16:02:27 -0400

    fixed glitch

--- onhover.gd
+++ onhover.gd
@@ -1,8 +1,9 @@
 extends Node3D
 
 var hovered: bool = false
-var clicked: bool = false
+var active: bool = false
 var highlight_color: Color = Color(1.0,0.0,0,1.0)
+var base_color: Color = Color(0.0,0.0,0,1.0)
 
 func _mouse_entered() -> void:
 	hovered = true
@@ -10,14 +11,15 @@ func _mouse_entered() -> void:
 
 func _mouse_exited() -> void:
 	hovered = false
-	set_mesh_color(Color(1.0,1.0,0,1.0))
+	set_mesh_color(base_color)
 	
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("mouse_click"):
+	if Input.is_action_just_pressed("mouse_click"):
+		print("pressed, active:", active)
 		if (hovered): 
-			clicked = !clicked
-			if (clicked): 
-				highlight_color = Color(1.0,0.0,1.0,1.0)
+			active = !active
+			if (active): 
+				highlight_color = Color.YELLOW
 				set_mesh_color(highlight_color)
 			else: 
 				highlight_color = Color.RED
