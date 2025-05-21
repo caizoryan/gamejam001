76ae7ea0c5dac06a7c2b1dc5bd683f6f94ba08bb
Aaryan Pashine <pashineaaryan@gmail.com>
2025-05-21 18:06:38 -0400

    updates

--- state_enums.gd
+++ state_enums.gd
@@ -5,4 +5,4 @@ class_name StateEnums
 enum Direction {X_AXIS, Z_AXIS, NONE = -1}
 
 # what direction in traffic light is currently green
-enum TrafficLight {LEFT_RIGHT, UP_DOWN, LR_TURN, UD_TURN, NONE = -1}
+enum TrafficLight {LEFT, RIGHT, UP, DOWN, NONE = -1}
