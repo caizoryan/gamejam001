5f0901daac11046c47197f9948f83dec39d15812
Aryaman Gupta <aryamangupta2002@gmail.com>
2025-05-22 00:47:17 -0400

    traffic light controls spawn + major cleanup

--- spawner.gd
+++ spawner.gd
@@ -1,11 +1,16 @@
 extends Node3D
 
-# already linked in object
-@export var SPAWN_POINT: MeshInstance3D
-@export var END_POINT: MeshInstance3D
-@export var FACING: StateEnums.Direction
-@export var CAR: PackedScene = preload("res://default_car.tscn")
-@export var TRAFFIC_LIGHT: PackedScene
+@export var TrafficLight: Node3D
+@export var LeftRoad: Node3D
+@export var RightRoad: Node3D
+@export var UpRoad: Node3D
+@export var DownRoad: Node3D
+var RoadDict
+var SpawnDict
+var EndDict
+var CarGroups
+
+@export var CAR: PackedScene = preload("res://car.tscn")
 
 # spawner variables
 # how many cars spawn at the same time
@@ -27,33 +32,70 @@ var cars = []
 ## global vars
 #var CAR_GROUP = Node3D.new()
 #var RUN_CARS = false
-var CURR_TRAFFIC_DIR = null
+var CURR_TRAFFIC_DIR = StateEnums.Direction.NONE
 #var SPAWN_CARS = false
 #var INIT_POSITION = Vector3(0,0,0)
 
 func _ready():
-	if TRAFFIC_LIGHT:
-		TRAFFIC_LIGHT.connect("light_changed", handle_light_change)
-		print("CAR", CAR)
-	var new_car = CAR.instantiate()
-	new_car.global_position = SPAWN_POINT.global_position
-	SPAWN_POINT.get_parent().add_child(new_car)
-	new_car.look_at(END_POINT.global_position, Vector3.UP)
-	cars.append(new_car)
-	print("END POS", END_POINT.global_position)
+	if TrafficLight:
+		TrafficLight.connect("light_changed", handle_light_change)
+
+	RoadDict = {
+		StateEnums.Direction.LEFT: LeftRoad,
+		StateEnums.Direction.RIGHT: RightRoad,
+		StateEnums.Direction.UP: UpRoad,
+		StateEnums.Direction.DOWN: DownRoad,
+	}
+	
+	SpawnDict = {
+		StateEnums.Direction.LEFT: LeftRoad.get_child(0),
+		StateEnums.Direction.RIGHT: RightRoad.get_child(0),
+		StateEnums.Direction.UP: UpRoad.get_child(0),
+		StateEnums.Direction.DOWN: DownRoad.get_child(0)
+	}
+	
+	EndDict = {
+		StateEnums.Direction.LEFT: LeftRoad.get_child(1),
+		StateEnums.Direction.RIGHT: RightRoad.get_child(1),
+		StateEnums.Direction.UP: UpRoad.get_child(1),
+		StateEnums.Direction.DOWN: DownRoad.get_child(1)
+	}
+	
+	CarGroups = {
+		StateEnums.Direction.LEFT: [],
+		StateEnums.Direction.RIGHT: [],
+		StateEnums.Direction.UP: [],
+		StateEnums.Direction.DOWN: []
+	}
 
 func _process(delta):
-	for car in cars:
-		if car.global_position.distance_to(END_POINT.global_position) < 1.0:
+	if CURR_TRAFFIC_DIR == StateEnums.Direction.NONE:
+		return
+	var end = EndDict[CURR_TRAFFIC_DIR]
+	for car in CarGroups[CURR_TRAFFIC_DIR]:
+		if car.global_position.distance_to(end.global_position) < 1.0:
 			car.queue_free()
-			cars.erase(car)
+			CarGroups[CURR_TRAFFIC_DIR].erase(car)
 		#else:
-		var velocity = car.global_position.direction_to(END_POINT.global_position) * car.speed
+		var velocity = car.global_position.direction_to(end.global_position) * car.speed
 		car.global_position += velocity * delta
 
 func handle_light_change(new_dir):
-	CURR_TRAFFIC_DIR = new_dir
-	
+	if CURR_TRAFFIC_DIR == StateEnums.Direction.NONE:
+		CURR_TRAFFIC_DIR = new_dir
+		RoadDict[CURR_TRAFFIC_DIR].run_cars = true
+	else:	
+		RoadDict[CURR_TRAFFIC_DIR].run_cars = false
+		CURR_TRAFFIC_DIR = new_dir
+		RoadDict[CURR_TRAFFIC_DIR].run_cars = true
+
+	var spawn = SpawnDict[CURR_TRAFFIC_DIR]
+	var end = EndDict[CURR_TRAFFIC_DIR]
+	var new_car = CAR.instantiate()
+	new_car.global_position = spawn.global_position
+	spawn.get_parent().add_child(new_car)
+	new_car.look_at(end.global_position, Vector3.UP)
+	CarGroups[CURR_TRAFFIC_DIR].append(new_car)
 	
 
 #func _process(delta):
--- state_enums.gd
+++ state_enums.gd
@@ -1,8 +1,5 @@
 extends Node
 class_name StateEnums
 
-# general purpose directions
-enum Direction {X_AXIS, Z_AXIS, NONE = -1}
-
 # what direction in traffic light is currently green
-enum TrafficLight {LEFT, RIGHT, UP, DOWN, NONE = -1}
+enum Direction {LEFT, RIGHT, UP, DOWN, NONE = -1}
